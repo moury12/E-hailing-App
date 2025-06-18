1888c9429186fb049254a94e4bf268c3f6f2b42d
moury12 <tanzibamouri00@gmail.com>
2025-06-18 15:23:20 +0600

    didn't fix yet map zoom when polyline show

--- lib/presentations/home/widgets/google_map_widget.dart
+++ lib/presentations/home/widgets/google_map_widget.dart
@@ -1,5 +1,6 @@
 import 'package:e_hailing_app/presentations/splash/controllers/common_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../../navigation/controllers/navigation_controller.dart';
@@ -25,41 +26,43 @@ class _GoogleMapWidgetState extends State<GoogleMapWidget> {
   @override
   Widget build(BuildContext context) {
     // logger.d("---------------------------------------");
-    return GoogleMap(
-      zoomGesturesEnabled: true,
-      scrollGesturesEnabled: true,
-      polylines: NavigationController.to.routePolylines,
+    return Obx(() {
+      return GoogleMap(
+        zoomGesturesEnabled: true,
+        scrollGesturesEnabled: true,
+        polylines: NavigationController.to.routePolylines,
 
-      onMapCreated: CommonController.to.onMapCreated,
-      initialCameraPosition: CameraPosition(
-        target: CommonController.to.marketPosition.value,
-        zoom: 13,
-      ),
-      myLocationEnabled: true,
-      myLocationButtonEnabled: false,
-      zoomControlsEnabled: true,
-      markers: {
-        Marker(
-          markerId: const MarkerId("selected_location"),
-          position: CommonController.to.marketPosition.value,
-          draggable: false,
-          onTap: () {
-            NavigationController.to.markerDraging.value = true;
-          },
-          onDragStart: (value) {
-            NavigationController.to.markerDraging.value = true;
-          },
-          onDragEnd: (value) {
-            CommonController.to.marketPosition.value = value;
-            NavigationController.to.getPlaceName(value);
-            NavigationController.to.markerDraging.value = false;
-          },
-          infoWindow: const InfoWindow(
-            title: "Selected Location",
-            snippet: "This is the chosen spot.",
-          ),
+        onMapCreated: CommonController.to.onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: CommonController.to.marketPosition.value,
+          zoom: 13,
         ),
-      },
-    );
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
+        zoomControlsEnabled: true,
+        markers: {
+          Marker(
+            markerId: const MarkerId("selected_location"),
+            position: CommonController.to.marketPosition.value,
+            draggable: false,
+            onTap: () {
+              NavigationController.to.markerDraging.value = true;
+            },
+            onDragStart: (value) {
+              NavigationController.to.markerDraging.value = true;
+            },
+            onDragEnd: (value) {
+              CommonController.to.marketPosition.value = value;
+              NavigationController.to.getPlaceName(value);
+              NavigationController.to.markerDraging.value = false;
+            },
+            infoWindow: const InfoWindow(
+              title: "Selected Location",
+              snippet: "This is the chosen spot.",
+            ),
+          ),
+        },
+      );
+    });
   }
 }
--- lib/presentations/my-rides/widgets/history_list_structure_widget.dart
+++ lib/presentations/my-rides/widgets/history_list_structure_widget.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'my_rides_history_card_item_widget.dart';
 
 class HistoryListStructureWidget extends StatelessWidget {
   final int length;
-  const HistoryListStructureWidget({
-    super.key, required this.length,
-  });
+
+  const HistoryListStructureWidget({super.key, required this.length});
 
   @override
   Widget build(BuildContext context) {
     return Column(
-
-      children: List.generate(length, (index) => MyRidesHistoryCardItemWidget()),
+      children: List.generate(
+        length,
+        (index) => MyRidesHistoryCardItemWidget(),
+      ),
     );
   }
 }
