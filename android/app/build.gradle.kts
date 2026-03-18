d6d1adb5e4a99eced43c5998158b35fe25333454
moury12 <tanzibamouri00@gmail.com>
2026-03-18 14:56:20 +0600

    feat: done work before eid

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.dudu.android.app"
         minSdk = 24
         targetSdk = 36 // or flutter.targetSdkVersion
-        versionCode = 15 // or flutter.versionCode
-        versionName = "1.1.6" // or flutter.versionName
+        versionCode = 16 // or flutter.versionCode
+        versionName = "1.1.7" // or flutter.versionName
         multiDexEnabled = true  // Add this
 
         manifestPlaceholders["MAPS_API_KEY"] = keystoreProperties.getProperty("MAPS_API_KEY") ?: ""
